32fee9486ca1ab55814ed8726007fdfe54b756ab
Jordan Lee <jordan@jordanlee.net>
2019-09-16 14:59:34 +1000

    First commit

---
+++ JordanDemoApi/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JordanDemoApi.Controllers
+{
+    [Route("")]
+    public class TestController : Controller
+    {
+        [HttpGet("testget1")]
+        public IActionResult TestGet()
+        {
+            return Ok();
+        }
+
+        [HttpGet("testget2")]
+        public IActionResult TestGet2([FromQuery]string name)
+        {
+            return Ok($"Hello {name}");
+        }
+
+        [HttpPost("testpost1")]
+        public IActionResult TestPost([FromBody]TestModel message)
+        {
+            return Ok(message);
+        }
+
+        [HttpPost("testpost2")]
+        public IActionResult TestPost2([FromQuery]string message)
+        {
+            return Ok(message);
+        }
+    }
+
+    public class TestModel
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ JordanDemoApi/FcRemoteEntrypoint.cs
@@ -0,0 +1,13 @@
+﻿using Aliyun.Serverless.Core.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace JordanDemoApi
+{
+    public class FcRemoteEntrypoint : FcHttpEntrypoint
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
